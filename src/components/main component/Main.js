24be2cdf0d51e7e8f172e1fb857a5a6f2b2447b4
Ivan Hancko <ivanhancko@yahoo.com>
2023-09-12 11:38:47 +0200

    create navbar and main, project is finish

--- src/components/main component/Main.js
+++ src/components/main component/Main.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main goes here!</div>
+    <main>
+        <h1 className='main--title'>Fun facts about React</h1>
+        <ul className='main--list'>
+            <li>Was first relased in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHUn</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
+
   )
 }
 
--- src/components/navbar component/Navbar.js
+++ src/components/navbar component/Navbar.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar goes here!</div>
+  
+    <nav>
+        <img  src="../logo192.png" alt='logo' className='nav--img' />
+        <h3 className='nav--logo_text'>ReactFacts</h3>
+        <h4 className='nav--text'>React Course - Project 1</h4>
+    </nav>
   )
-}
+};
 
-export default Navbar
+export default Navbar;
